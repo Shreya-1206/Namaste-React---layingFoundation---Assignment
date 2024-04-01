1856496f71085ef67b7de00e9c1d160ececb4dcd
Shreya <shreyaray.1206@gmail.com>
2024-04-01 17:58:18 +0530

    laying-foundation-assignment

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("div" , {id :"main"}, [React.createElement("h1", {id : "h1"}, "Namaste"),
+React.createElement("h2", {id : "h2"}, "React"), React.createElement("h3", {id : "h3"}, "❤️")
+]);
+
+const withJSX = (
+    <div>
+        <h1>Namaste</h1>
+        <h1>React</h1>
+        <h1>❤️</h1>
+    </div>
+);
+
+const withFunctionComponent = () => {
+    return (
+        <div>
+         <h1>Namaste</h1>
+         <h1>React</h1>
+         <h1>❤️</h1>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(withFunctionComponent());
